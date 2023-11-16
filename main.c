5d783a1e874eb82608038fe92b8c1e263c732a09
marvitian <80377652+marvitian@users.noreply.github.com>
2023-11-15 23:25:00 -0500

    updated main

--- main.c
+++ main.c
@@ -10,18 +10,12 @@
 #include <string.h>
 #include <stdio.h>
 #include "GLCD.h"
-//#define osObjectsPublic                     // define objects in main module
-//#include "osObjects.h"                      // RTOS object definitions
-#ifdef led
-#include "LED.h"
-#endif
 #include "LPC17xx.h"
 #include "KBD.h"
 
 #define __use_LCD 1
 #define __FI 1
 
-// define joystick input up=0008 down=0020 left=0040 right=0010 select=0001
 #define UP 0x08
 #define DOWN 0x20
 #define LEFT 0x40
@@ -29,19 +23,24 @@
 #define SELECT 0x01
 #define REST 0x00
 
-    //define main menu options
-    typedef enum{
+
+
+
+
+// #########################################################
+// ####       PROGRAM START                             ####     
+// #########################################################
+
+//define main menu options
+typedef enum{
         OPTION_1,
         OPTION_2,
         OPTION_3,
         OPTION_4,
         MENU_SIZE
-    } MENU_Item;
+} MENU_Item;
 
-		MENU_Item current_item = OPTION_1;
-// prototype functions
-void inputController(void const *argument);
-void menuController();
+MENU_Item current_item = OPTION_1;
 #define MAIN_MENU(OPTION) do { \
     GLCD_SetBackColor(White); \
     GLCD_SetTextColor(Black); \
@@ -85,51 +84,32 @@ void menuController();
 } while (0)
 
 
+// prototype functions
+void inputController(void const *argument);
+void menuController();
+
+// mutex
 osMutexDef(lcd_mutex);
 osMutexId(lcd_mutexID);
-/*
-        #ifdef __use_LCD
-						#ifdef led
-            LED_Out(8);
-						#endif
-            sprintf(text, "task d status: %f", result);
-            osMutexWait(lcd_mutexID, osWaitForever);
-            GLCD_SetTextColor(Black);    
-            GLCD_DisplayString(3, 0, __FI, "");
-            GLCD_DisplayString(8, 0, __FI, (unsigned char *) text);
-            osMutexRelease(lcd_mutexID);
-        #endif
-
-*/
 
 osThreadId inputControllerID;
 osThreadDef(inputController,osPriorityNormal, 1, 0);
-/*
- * main: initialize and start the system
- */
+
 int main (void) {
+    // lcd mutex not in use*
+	lcd_mutexID = osMutexCreate(osMutex(lcd_mutex));
 	
-	int in;
-	  KBD_Init();			/* KBD initialization */
-	// prepare lcd mutex
 	
-	    lcd_mutexID = osMutexCreate(osMutex(lcd_mutex));
-	
-	    // Prepare LCD
+	    
     #ifdef __use_LCD
+        KBD_Init();			/* KBD initialization */
         GLCD_Init();                               /* Initialize graphical LCD (if enabled */
-        #ifdef led
-				LED_Init();
-				#endif
         GLCD_Clear(White);                         /* Clear graphical LCD display   */
         GLCD_SetBackColor(Blue);
         GLCD_SetTextColor(Yellow);
         GLCD_DisplayString(0, 0, __FI, "  media centre ");
-        //	GLCD_SetTextColor(White);
-        //	GLCD_DisplayString(1, 0, __FI, "Lab Demo #4");
         GLCD_SetTextColor(White);
         MAIN_MENU(1); //instantiate main menu
-    
     #endif
 		
 		
@@ -147,174 +127,94 @@ int main (void) {
     // ##################
     // testing region 
     // ##################
-    int InputBuffer[100];
-    int InputBufferIndex = 0;
+
+//not currently used: 
+int InputBuffer[100];
+int InputBufferIndex = 0;
 
 
-//threads
 
 
 // input thread
-    void inputController(void const *argument){
-			while(1){
-        int input = get_button();
-				char text[10];
-				sprintf(text,"%x", input);
-				//printf("%x, %d", input, input);
-				GLCD_DisplayString(8, 0, __FI, (unsigned char *) text);
+void inputController(void const *argument){
+    while(1)
+    {
+        #ifdef __use_LCD
+            int input = get_button();
+            char text[10];
+            sprintf(text,"%x", input);
+            //printf("%x, %d", input, input);
+            GLCD_DisplayString(8, 0, __FI, (unsigned char *) text);
 
-				if(input != REST){
-             // display menu
-            // check input
-            // if input is select, then run the current menu item
-            // if input is up, then move up the menu
-            // if input is down, then move down the menu
-            // if input is left, then move left the menu
-            // if input is right, then move right the menu
-            
-            switch(input){
-                case UP:
-                    current_item = (current_item - 1) % MENU_SIZE;
-                    break;
-                case DOWN:
-                    current_item = (current_item + 1) % MENU_SIZE;
-                    break;
-                case SELECT:
-                    switch(current_item){
-                        case OPTION_1:
+            if(input != REST)
+            {
+                switch(input)
+                {
+                    case UP:
+                        current_item = (current_item - 1) % MENU_SIZE;
+                        break;
+                    case DOWN:
+                        current_item = (current_item + 1) % MENU_SIZE;
+                        break;
+                    case SELECT:
+                        switch(current_item)
+                        {
+                            case OPTION_1:
+                                // run option 1
+                                MAIN_MENU(1);				
+                                break;
+                            case OPTION_2:
+                                // run option 2
+                                MAIN_MENU(2);							
+                                break;
+                            case OPTION_3:
+                                // run option 3
+                                MAIN_MENU(3);
+                                break;
+                            case OPTION_4:
+                                // run option 4
+                                MAIN_MENU(4);
+                                break;
+                            default:
+                                // do nothing
+                                break;
+                        }
+                        break;
+                    default:
+                    // do nothing
+                        break;
+                }   
+                switch(current_item)
+                {
+                    case OPTION_1:
                             // run option 1
                             MAIN_MENU(1);
-														osDelay(10);
+                            
                             break;
-                        case OPTION_2:
+                    case OPTION_2:
                             // run option 2
                             MAIN_MENU(2);
-														osDelay(10);
-														break;
-                        case OPTION_3:
+                            
+                            break;
+                    case OPTION_3:
                             // run option 3
                             MAIN_MENU(3);
+                            
                             break;
-                        case OPTION_4:
+                    case OPTION_4:
                             // run option 4
                             MAIN_MENU(4);
+                            
                             break;
-                        default:
+                    default:
                             // do nothing
                             break;
-                    }
-                    break;
-                default:
-                    // do nothing
-                    break;
+                }
+        
             }
-						switch(current_item){
-								case OPTION_1:
-										// run option 1
-										MAIN_MENU(1);
-										
-										break;
-								case OPTION_2:
-										// run option 2
-										MAIN_MENU(2);
-										
-										break;
-								case OPTION_3:
-										// run option 3
-										MAIN_MENU(3);
-										
-										break;
-								case OPTION_4:
-										// run option 4
-										MAIN_MENU(4);
-										
-										break;
-								default:
-										// do nothing
-										break;
-						}
-        
-        }
-        InputBuffer[InputBufferIndex] = input;
-        InputBufferIndex = (InputBufferIndex + 1) % 100;
-			}
+            //not currently used:
+            InputBuffer[InputBufferIndex] = input;
+            InputBufferIndex = (InputBufferIndex + 1) % 100;
+        #endif
     }
-
-    // control state thread
-    void controlStateController(){
-        // read input buffer
-        // update menu state    
-        
-    }
-
-
-
-
-
-//functions 
-
-    /*
-
-    // might have to change the type of input
-    int menuController(int input){
-        MENU_Item current_item = OPTION_1;
-        
-            // display menu
-            // check input
-            // if input is select, then run the current menu item
-            // if input is up, then move up the menu
-            // if input is down, then move down the menu
-            // if input is left, then move left the menu
-            // if input is right, then move right the menu
-           // int input = input;
-            switch(input){
-                case UP:
-                    current_item = (current_item + 1) % MENU_SIZE;
-                    break;
-                case DOWN:
-                    current_item = (current_item - 1) % MENU_SIZE;
-                    break;
-                case SELECT:
-                    switch(current_item){
-                        case OPTION_1:
-                            // run option 1
-                            break;
-                        case OPTION_2:
-                            // run option 2
-                            break;
-                        case OPTION_3:
-                            // run option 3
-                            break;
-                        case OPTION_4:
-                            // run option 4
-                            break;
-                        default:
-                            // do nothing
-                            break;
-                    }
-                    break;
-                default:
-                    // do nothing
-                    break;
-            }
-        
-    }
-    
-		*/
-
-	// testing region
-    /*
-		checkInput(){
-            // figure out handling joystick input
-			char text[10];
-			int dir = KBD_get();
-			int dir2 = get_button();
-			sprintf(text,"0x%04X", dir2);
-            #ifdef __use_LCD
-                osMutexWait(lcd_mutexID, osWaitForever);
-			    GLCD_DisplayString(4, 0, __FI, (unsigned char *) text);
-                osMutexRelease(lcd_mutexID);
-            #endif
-
-		}
-    */
+}
